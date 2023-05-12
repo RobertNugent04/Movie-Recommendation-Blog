bb42cc9b354baf1c2447c4bfa1e2e98e7eccf500
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-05-12 19:26:21 +0100

    Searh Result Fully Integrated and Functional

--- public/js/search.js
+++ public/js/search.js
@@ -4,60 +4,32 @@ document.addEventListener('DOMContentLoaded', function () {
 
     searchButton.addEventListener('click', event => {
         event.preventDefault();
-        performSearch(searchInput.value);
+        performSearch(searchInput.value, 1);
     });
 
-    searchInput.addEventListener('keyup', event => {
-        if (event.key === 'Enter') {
-            event.preventDefault();
-            performSearch(searchInput.value);
+    searchInput.addEventListener('input', event => {
+        event.preventDefault();
+        if (searchInput.value.trim() !== '') {
+            performSearch(searchInput.value, 1);
+        } else {
+            // Clear results if input is empty
+            const searchResultsContainer = document.getElementById('search-results-container');
+            searchResultsContainer.innerHTML = '';
         }
     });
 });
 
-
-async function performSearch(query) {
+async function performSearch(query, page) {
     const apiKey = '8295dbb4116ffc5b458cb9378ac368ea';
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}`;
-    // const url = `https://api.themoviedb.org/3/movie/550?api_key=8295dbb4116ffc5b458cb9378ac368ea&query=${encodeURIComponent(query)}`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}&page=${page}`;
 
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data.results);  // Add this line
 
-
-    displayResults(data.results);
+    displayResults(data.results, data.total_pages, query, page);
 }
 
-// function displayResults(movies) {
-//     // Clear previous search results
-//     const searchResultsContainer = document.getElementById('search-results');
-//     if (searchResultsContainer) {
-//         searchResultsContainer.remove();
-//     }
-
-//     // Create a container for the search results
-//     const newSearchResultsContainer = document.createElement('div');
-//     newSearchResultsContainer.id = 'search-results';
-//     newSearchResultsContainer.className = 'container mt-3';
-
-//     // Iterate through movies and display them
-//     for (const movie of movies) {
-//         const movieElement = document.createElement('div');
-//         movieElement.className = 'search-result';
-
-//         const movieLink = document.createElement('a');
-//         movieLink.href = `/movies/${movie.id}`;
-//         movieLink.innerText = movie.title;
-
-//         movieElement.appendChild(movieLink);
-//         newSearchResultsContainer.appendChild(movieElement);
-//     }
-
-//     document.body.appendChild(newSearchResultsContainer);
-// }
-
-function displayResults(movies) {
+function displayResults(movies, totalPages, query, currentPage) {
     // Get the search results container
     const searchResultsContainer = document.getElementById('search-results-container');
 
@@ -70,48 +42,99 @@ function displayResults(movies) {
     newSearchResultsContainer.className = 'container mt-3';
 
     // Iterate through movies and display them
-    for (const movie of movies) {
+    for (const movie of movies.slice(0, 3)) {
         const movieElement = document.createElement('div');
-        movieElement.className = 'card mb-3';
+        movieElement.className = 'media border p-3 mb-2';
 
+        const img = document.createElement('img');
         if (movie.poster_path) {
-            const img = document.createElement('img');
             img.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
-            img.alt = movie.title;
-            img.className = 'card-img-top';
-            movieElement.appendChild(img);
         } else {
-            // Optional: Add a default image or some placeholder content here
-            const img = document.createElement('img');
             img.src = `https://www.nbmchealth.com/wp-content/uploads/2018/04/default-placeholder.png`;
-            img.alt = movie.title;
-            img.className = 'card-img-top';
-            movieElement.appendChild(img);
         }
+        img.alt = movie.title;
+        img.className = 'mr-3 align-self-start';
+        img.style.width = '60px';
+        movieElement.appendChild(img);
 
-        const cardBody = document.createElement('div');
-        cardBody.className = 'card-body';
+        const mediaBody = document.createElement('div');
+        mediaBody.className = 'media-body';
 
+        // const movieLink = document.createElement('a');
+        // movieLink.href = `/movies/${movie.id}`;
+        // movieLink.innerText = movie.title;
+        // movieLink.className = 'mt-0';
+        // mediaBody.appendChild(movieLink);
         const movieLink = document.createElement('a');
         movieLink.href = `/movies/${movie.id}`;
         movieLink.innerText = movie.title;
-        movieLink.className = 'card-title';
-        cardBody.appendChild(movieLink);
+        movieLink.className = 'mt-0 movie-title-link';  // Add class for styling
+        mediaBody.appendChild(movieLink);
 
-        const movieYear = document.createElement('p');
+        const movieYear = document.createElement('h6');
         movieYear.innerText = movie.release_date.split('-')[0];
-        movieYear.className = 'card-subtitle mb-2 text-muted';
-        cardBody.appendChild(movieYear);
+        mediaBody.appendChild(movieYear);
 
+        // const movieOverview = document.createElement('p');
+        // movieOverview.innerText = movie.overview;
+        // mediaBody.appendChild(movieOverview);
         const movieOverview = document.createElement('p');
         movieOverview.innerText = movie.overview;
-        movieOverview.className = 'card-text';
-        cardBody.appendChild(movieOverview);
+        movieOverview.className = 'movie-overview';  // Add class for styling
+        mediaBody.appendChild(movieOverview);
 
-        movieElement.appendChild(cardBody);
+        // Create the "See more" button
+        const seeMoreButton = document.createElement('a');
+        seeMoreButton.href = `/movies/${movie.id}`;
+        seeMoreButton.innerText = 'See more';
+        seeMoreButton.className = 'btn';
+        seeMoreButton.style.backgroundColor = '#040012';
+        seeMoreButton.style.color = 'white';
+        mediaBody.appendChild(seeMoreButton);
+
+        movieElement.appendChild(mediaBody);
         newSearchResultsContainer.appendChild(movieElement);
     }
 
+    // Create pagination
+    const pagination = document.createElement('ul');
+    pagination.className = 'pagination justify-content-center mt-4';
+
+    const maxPagesToShow = 5;  // Maximum number of pages to display at once
+    const startPage = Math.max(1, currentPage - Math.floor(maxPagesToShow / 2));
+    const endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
+
+    // Add "First" page button
+    pagination.appendChild(createPageLink(1, 'First', query, currentPage === 1));
+
+    for (let i = startPage; i <= endPage; i++) {
+        pagination.appendChild(createPageLink(i, i.toString(), query, i === currentPage));
+    }
+
+    // Add "Last" page button
+    pagination.appendChild(createPageLink(totalPages, 'Last', query, currentPage === totalPages));
+
+    newSearchResultsContainer.appendChild(pagination);
+
     // Append new search results container to the existing one
     searchResultsContainer.appendChild(newSearchResultsContainer);
+}
+
+function createPageLink(page, text, query, isActive = false) {
+    const pageLink = document.createElement('li');
+    pageLink.className = 'page-item';
+    if (isActive) {
+        pageLink.classList.add('active'); // Highlight current page
+    }
+    const link = document.createElement('a');
+    link.className = 'page-link';
+    link.href = '#';
+    link.innerText = text;
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        performSearch(query, page);
+    });
+    pageLink.appendChild(link);
+
+    return pageLink;
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,102 +1,147 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
     
-    <!-- Styles -->
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/2d3df2f795.js" crossorigin="anonymous"></script>
+  <!-- Styles -->
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/2d3df2f795.js" crossorigin="anonymous"></script>
 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <style>
-      .dropdown-menu {
-        border: none;
-        border-radius: 0;
-        background:#040012;
-      }
+  <style>
+  .dropdown-menu {
+    border: none;
+    border-radius: 0;
+    background:#040012;
+  }
+  .dropdown-item {
+    color: #ffffff;
+  }
       
-      .dropdown-item {
-        color: #ffffff;
-      }
+  .dropdown-item:hover {
+    color: #040012;
+    background: #ffffff;
+  }
       
-      .dropdown-item:hover {
-        color: #040012;
-        background: #ffffff;
-      }
-      
-      .dropdown-item:not(:last-child) {
-        border-bottom: 1px solid #6c757d;
-      }
+  .dropdown-item:not(:last-child) {
+    border-bottom: 1px solid #6c757d;
+  }
 
-      .search-result {
-  background-color: #f8f9fa;
-  padding: 10px;
-  margin-bottom: 5px;
-  border-radius: 5px;
-}
+  .search-result {
+    background-color: #f8f9fa;
+    padding: 10px;
+    margin-bottom: 5px;
+    border-radius: 5px;
+  }
 
-.search-result:hover {
-  background-color: #e9ecef;
-}
+    .search-result:hover {
+      background-color: #e9ecef;
+    }
 
-      </style>
+  .media {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+  }
+  .media img {
+    margin-right: 10px;
+  }
+  .media .media-body {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .movie-title-link {
+    text-decoration: none;  /* remove underline */
+    color: #040012;  /* dark color for title */
+    font-weight: bold;  /* make title bold */
+  }
+
+  .movie-title-link:hover {
+    color: #040012;  /* keep dark color on hover */
+  }
+
+  .movie-overview {
+    color: #6c757d;  /* dark grey color for overview text */
+    font-size: 0.9em;  /* slightly smaller font size for overview text */
+    line-height: 1.5;  /* increase line height for readability */
+    margin-top: 10px;  /* add some top margin for separation */
+  }
+
+  .page-link {
+    color: #fff; /* White text */
+    background-color: #040012; /* Dark background */
+    border-color: #040012; /* Dark border */
+  }
+
+  .page-link:hover {
+    color: #040012; /* Dark text on hover */
+    background-color: #fff; /* White background on hover */
+    border-color: #040012; /* Dark border on hover */
+  }
+
+  .page-item.active .page-link {
+    color: #040012; /* Dark text for active link */
+    background-color: #fff; /* White background for active link */
+    border-color: #040012; /* Dark border for active link */
+  }
+  </style>
 </head>
 <body class="bg-gray-100 h-screen antialiased leading-none font-sans">
-    <div id="app">
-      <header style="background-color: #040012"  class="py-1">
-        <div class="container">
-          <nav class="navbar navbar-expand-lg navbar-dark">
-            <div class="d-flex align-items-center">
-              <img src="{{ asset('images/logo.png') }}" alt="Logo" class="h-4 w-4 mr-2" height="70" width="70">
-              <a class="navbar-brand" href="{{ url('/') }}">RobPat</a>
-            </div>
+  <div id="app">
+    <header style="background-color: #040012"  class="py-1">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-dark">
+          <div class="d-flex align-items-center">
+            <img src="{{ asset('images/logo.png') }}" alt="Logo" class="h-4 w-4 mr-2" height="70" width="70">
+            <a class="navbar-brand" href="{{ url('/') }}">RobPat</a>
+          </div>
       
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
       
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-              <form class="d-flex mx-auto w-50 my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button style="border: none" class="btn btn-outline-light my-sm-0" type="submit"><i class="fa-solid fa-magnifying-glass"></i></i></button>
-              </form>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <form class="d-flex mx-auto w-50 my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+              <button style="border: none" class="btn btn-outline-light my-sm-0" type="submit"><i class="fa-solid fa-magnifying-glass"></i></i></button>
+            </form>
 
-      
-              <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                  <a class="nav-link" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/movies">Movies</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/blog">Reviews</a>
-                </li>
-                @guest
-                  <li class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                  </li>
-                  @if (Route::has('register'))
-                    <li class="nav-item">
-                      <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                    </li>
-                  @endif
-                @else
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    {{ Auth::user()->name }}
-                  </a>
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/movies">Movies</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/blog">Reviews</a>
+              </li>
+              @guest
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+              </li>
+              @if (Route::has('register'))
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+              </li>
+              @endif
+              @else
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                {{ Auth::user()->name }}
+                </a>
                   <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                     <li>
                       <a class="dropdown-item" href="/blog">User Dashboard</a>
@@ -108,35 +153,34 @@
                       <a class="dropdown-item" href="/blog">My Reviews</a>
                     </li>
                     <li>
-                      <a class="dropdown-item" href="{{ route('logout') }}"
+                    <a class="dropdown-item" href="{{ route('logout') }}"
                          onclick="event.preventDefault();
                                   document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
-                      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
-                        {{ csrf_field() }}
-                      </form>
-                    </li>
-                  </ul>
-                </li>
-                @endguest
-              </ul>
-            </div>
-          </nav>
-        </div>
-      </header>
-
-      <!-- this is where the search results will be inserted -->
-<div id="search-results-container"></div>
-
-        <div>
-            @yield('content')
-        </div>
-
-        <div>
-            @include('layouts.footer')
-        </div>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                      {{ csrf_field() }}
+                    </form>
+                  </li>
+                </ul>
+              </li>
+              @endguest
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+    
+    <!-- this is where the search results will be inserted -->
+    <div id="search-results-container"></div>
+    
+    <div>
+      @yield('content')
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('js/search.js') }}" defer></script>
-
+    
+    <div>
+      @include('layouts.footer')
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="{{ asset('js/search.js') }}" defer></script>
 </body>
 </html>
