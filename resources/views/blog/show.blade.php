aa43bcbfd1268c282a5a9a4316694db6f54215e3
Robert Nugent <d00244618@student.dkit.ie>
2023-05-13 21:18:13 +0100

    Improved design of individual blogs page

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,42 +1,39 @@
-@extends('layouts.blogApp')
+@extends('layouts.app')
 
 <style>
 
-h1{
-    color:white;
-}
 
 h2{
     color:slategray;
 }
 
-p{
-    color:white;
+.flex {
+    display: flex;
+    background: url('{{ asset('images/' . $post->image_path) }}') no-repeat center center fixed;
+    background-size: cover;
 }
 
-    </style>
+.content-container {
+    width: 50%;
+    background: rgba(255, 255, 255, 0.9);
+    padding: 5rem;
+    word-wrap: break-word;
+}
 
+</style>
 @section('content')
-<div class="w-4/5 m-auto text-left">
-    <div class="py-15">
-        <h1 class="text-6xl">
-            {{ $post->title }}
-        </h1>
-        <h2 class="text-xl">
-            About: {{ $post->movieName }}
-        </h2>
-        <p class="text-xl pt-8 pb-10 leading-8 font-light">
-            {{ $post->description }}
-        </p>
+
+<div class="flex">
+    <div class="content-container">
+        <h1 class="text-6xl">{{ $post->title }}</h1>
+        <h2 class="text-xl">About: {{ $post->movieName }}</h2>
+        <p class="text-xl pt-8 pb-10 leading-8 font-light">{{ $post->description }}</p>
+        <span class="text-gray-500 block">
+            By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
+        </span>
     </div>
 </div>
+@endsection
 
-<div class="w-4/5 m-auto pt-1">
 
-    <span class="text-gray-500">
-        By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
-    </span>
 
-</div>
-
-@endsection 
