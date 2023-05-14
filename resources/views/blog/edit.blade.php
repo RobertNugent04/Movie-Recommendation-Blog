5b25b6df7cbbcfd446cb19b344b1ddd8996dc619
Robert Nugent <d00244618@student.dkit.ie>
2023-05-14 15:59:29 +0100

    Added css to edit blog page

--- resources/views/blog/edit.blade.php
+++ resources/views/blog/edit.blade.php
@@ -3,25 +3,13 @@
 @section('content')
 <div class="w-4/5 m-auto text-left">
     <div class="py-15">
-        <h1 class="text-6xl">
+        <h1 class="text-6xl text-center">
             Update Post
         </h1>
     </div>
 </div>
 
-@if ($errors->any())
-    <div class="w-4/5 m-auto">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li class="w-1/5 mb-4 text-gray-50 bg-red-700 rounded-2xl py-4">
-                    {{ $error }}
-                </li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
-<div class="w-4/5 m-auto pt-20">
+<div class="m-auto bg-white rounded-lg shadow-lg p-8 md:w-3/4 lg:w-2/5" style="padding: 30px; max-width: 800px;">
     <form 
         action="/blog/{{ $post->slug }}"
         method="POST"
@@ -29,29 +17,55 @@
         @csrf
         @method('PUT')
 
+        <div class="flex flex-col mb-6">
+            <label for="title" class="mb-2 text-lg">Blog Title</label>
         <input 
             type="text"
             name="title"
             value="{{ $post->title }}"
-            class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
+            class="w-full px-4 py-2 border rounded-lg">
+            @error('title')
+            <p style="color:red;" class="text-red-700 text-lg font-bold">{{ $message }}</p>
+            @enderror
+        </div>
+        <br>
 
+        <div class="flex flex-col mb-6">
+            <label for="movieName" class="mb-2 text-lg">Subject</label>
             <input 
             type="text"
             name="movieName"
             value="{{ $post->movieName }}"
-            class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
+            class="w-full px-4 py-2 border rounded-lg"
+            >
+            @error('movieName')
+            <p style="color:red;" class="text-red-700 text-lg font-bold">{{ $message }}</p>
+            @enderror
+        </div>
+        <br>
 
+        <div class="flex flex-col mb-6">
         <textarea 
             name="description"
             placeholder="Description..."
-            class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none">{{ $post->description }}</textarea> 
+            class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none"
+            >{{ $post->description }}</textarea>
+            @error('description')
+            <p style="color:red;" class="text-red-700 text-lg font-bold">{{ $message }}</p>
+            @enderror
+        </div>
 
-        <button    
-            type="submit"
-            class="uppercase mt-15 bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
-            Submit Post
-        </button>
+
+        <div class="flex justify-center">
+            <button
+                type="submit"
+                class="uppercase mt-15 bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl" style="background-color: black"
+            >
+                Update Post
+            </button>
+        </div>
     </form>
 </div>
+<br><br>
 
 @endsection
